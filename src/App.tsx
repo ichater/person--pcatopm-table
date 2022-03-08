1abfd5c57fe1ca4c6a8f07ff9e00e366c0c377af
Izaak Chater <izaak.chater@rea-group.com>
2022-03-08 16:01:04 +1100

    accessed api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+// https://randomuser.me/api/?results=20
+
+
 function App() {
+  const [personData, setPersonData] = useState<any[]>([])
+
+
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch('https://randomuser.me/api/?results=20')
+      .then(response => response.json())
+      .then(data => setPersonData(data.results))
+      .catch(err => console.log(err))
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {personData.map(p => (<div>{p.name.title}</div>))}
+      {/* <button onClick={() => { console.log(personData[1]) }}>click me</button> */}
     </div>
   );
 }
